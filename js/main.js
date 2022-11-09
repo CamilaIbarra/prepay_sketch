54be994a1bbfcea31c10e711518513c703e289a2
Camila Ibarra <ibarra.camiailen9@gmail.com>
2022-11-09 20:24:33 -0300

    First commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// show sign up form when click 
+$('.signup-show').click(function(){
+    $('.register-form').show();
+    $('.login-form').hide();    
+});
+
+// show login form when click
+$('.login-show').click(function(){
+    $('.login-form').show();    
+    $('.register-form').hide();
+});
