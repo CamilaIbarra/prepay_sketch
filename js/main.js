73fe1c3150751279b1b3fbaa3afe8477cf9ce034
Camila Ibarra <ibarra.camiailen9@gmail.com>
2022-11-23 18:33:20 -0300

    added show and hide password

--- js/main.js
+++ js/main.js
@@ -9,3 +9,14 @@ $('.login-show').click(function(){
     $('.login-form').show();    
     $('.register-form').hide();
 });
+
+// show password
+const showPassword = document.querySelector("#show-password");
+const passwordField =document.querySelector("#floatingPassword");
+
+showPassword.addEventListener("click", function(){
+    this.classList.toggle("fa-eye");
+    const type = passwordField.getAttribute("type")
+    === "password" ? "text" : "password";
+    passwordField.setAttribute("type", type);
+});
